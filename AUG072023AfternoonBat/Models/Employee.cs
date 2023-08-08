4fc5febabd94d50ae3b23012312de587863eaa2d
elangrg <45620769+elangrg@users.noreply.github.com>
2023-08-08 14:48:30 +0530

    Add project files.

---
+++ AUG072023AfternoonBat/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace AUG072023AfternoonBat.Models
+{
+    public class Employee
+    {
+        public int EmpID { get; set; }
+        public string EmpName { get; set; }
+        public double Salary { get; set; }
+    }
+}
